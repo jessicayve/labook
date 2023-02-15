8e6452c16be3d17609ea2cc378636e9b0050886e
jessicayve <yve.jessica@gmail.com>
2023-02-15 15:59:22 -0300

    :sparkles: implementando token

---
+++ src/database/atualizarHash.sql
@@ -0,0 +1,20 @@
+-- Active: 1676291572353@@127.0.0.1@3306
+UPDATE users
+SET password = "$2y$12$CPAhYAV4iw10KgOqh.MnwOps6ADbWlqA/20eJ2L.ZTClwNhiQgqi2"
+WHERE name = "Duncan Jason";
+-- plaintext = duncan01
+
+UPDATE users
+SET password = "$2y$12$swzYA2HACTptC0F.jTjNLuw6riTSBuGp.dmNEM28Xkjz2RFu7JDe2"
+WHERE name = "Julio Matias";
+-- plaintext = julio02
+
+UPDATE users
+SET password = "$2y$12$83p07pXDpfVybJuzEdGo9u0qc/rlGGkAZ0y4b3rk47DFFnu4tV6k2"
+WHERE name = "Celeste Carolina";
+-- plaintext = celeste03
+
+UPDATE users
+SET password = "$2y$12$83p07pXDpfVybJuzEdGo9u0qc/rlGGkAZ0y4b3rk47DFFnu4tV6k2"
+WHERE name = "Valentina Pascal";
+-- plaintext = valentina04
